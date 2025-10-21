eb949c9aedcbc7e707b344731d6e7ea068e916cf
AndrewL789 <Andrewl434@nycstudents.net>
2025-10-21 09:26:28 -0400

    file 6 undone

--- app.py
+++ app.py
@@ -4,3 +4,42 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
+# file 1 
+""" for movies in data:
+    print(movies['title']) """
+
+#file 2
+""" gah = int(input('year? :')) 
+for tah in data:
+    if gah < tah['year']:
+        print(tah['title'], tah['year']") """
+
+""" #file 3
+low = int(input('lb?:'))
+uppa = int(input('ub?:'))
+for tree in data:
+    if (tree['year'] > low) and (tree['year']  < uppa):
+        print(tree['title'], tree['year']) """
+
+#file4 
+""" ball = int(input('when?:'))
+for smth in data:
+    if ball == smth['year']:
+        print(smth['title'], smth['year'])  """
+
+#file5
+""" ti = input('write :') 
+def find(x):
+    for g in data:
+        if x.lower() in g['title'].lower():
+            print(g['title'])
+find(ti) """
+
+#file6 genre
+billy = input('genre? :')
+def gen(x):
+    for la in data:
+        for things in la['genres']:
+            if things in la['genres']:
+                print(la['title'])
+gen(billy) 
